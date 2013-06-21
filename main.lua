14b30260b1aec7d19fef02fbfccca6479a2c7652
Chris <chris.dugne@mapnify.com>
2013-06-21 15:36:20 +0200

    conf prod

--- main.lua
+++ main.lua
@@ -10,8 +10,8 @@ APP_VERSION = "2.5"
 -----------------------------------------------------------------------------------------
 
 --SERVER_URL 		= "http://192.168.0.7:9000" 
-SERVER_URL 		= "http://localhost:9000" 
---SERVER_URL 		= "http://diligis.herokuapp.com/" 
+--SERVER_URL 		= "http://localhost:9000" 
+SERVER_URL 		= "http://diligis.herokuapp.com/" 
 
 -----------------------------------------------------------------------------------------
 
